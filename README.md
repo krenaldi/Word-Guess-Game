0397c4be5b1e475cff522d2f4ed4bf8599e1c534
Kris Renaldi <krenaldi@comcast.net>
2019-06-12 15:58:59 -0400

    Updated js file so that underscroes appear on page

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Word-Guess-Game
 Online version of Hangman
+
+
+# generate a word at random and store it in a variable
+# display the length of the word to the user
+# correct_guesses is less than the length of the word
+# prompt the user to guess a letter
+# if the guess is correct increment correct_guesses by 1
+# if the guess is incorrect increment incorrect_guesses by 1 
+# and draw the next part of the hangman
+# if the incorrect_guesses is greater than 8, tell the user 
+# they lost and exit the program
+# if correct_guesses is equal to the length of the word, tell the user they won
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,23 +6,77 @@ var words = ["Shinedown", "Rival Sons", "Greta Van Fleet"];
 var guessesLeft = 6;
 
 // Empty arrays that hold the keys the user pressed for guess  
-var rightGuess = [];
-var wrongGuess = []; 
+var matchedLetters = [];
+var guessedLetters = [];
 
 // Empty array to hold the number of underscores for new word
-var underScores = [];
+//var underScores = [];
 
 // Choose band from words array
 var randNum = Math.floor(Math.random() * words.length);
-var chosenWord = words[randNum];
+var chosenWord = words[randNum].toLowerCase();
+
+var underScores = chosenWord.split(" ");
+console.log(chosenWord);
 
 
+// Variables for DOM manipulation
+//var docUnderScore = document.querySelector("underscores");
+//var docRightGuess = document.querySelector("rightguess");
+//var docWrongGuess = document.querySelector("wrongguess");
 
 // ############ MAIN GAME ENGINE ##############
-// Create underscores based on length of word
-var generateUnderscore = function(){
-    for (var 1 = 0; i < chosenWord.length; i++){
-        underScores.push("_");
+// Create underscores based on length of word & update based on user's guesses
+function generateUnderscore() {
+    // Create empty string variable to hold the letters of chosenWord
+    var wordLetters = " ";
+
+    // Loop through the letters of the chosenWord
+    for (var i = 0; i < chosenWord.length; i++) {
+        // If the current letter has been guessed, then display that letter
+        if (matchedLetters.indexOf(chosenWord[i]) !== -1) {
+            wordLetters += chosenWord[i];
+        }
+        // If it hasn't been guessed then display an underscore
+        else {
+            wordLetters += "&nbsp;_&nbsp;";
+        }
     }
-    return underScores;
+    // Update the page with the new string we built.
+    document.querySelector("#underscores").innerHTML = wordLetters;
 }
+
+function gameStart() {
+    // Splits the word into individual letters
+    //underScores = chosenWord.split(" ");
+
+    // Builds the underscores based on length of the word
+    // generateUnderscore();
+}
+generateUnderscore();
+//console.log(generateUnderscore());
+
+// Event listener to save users choice and compare to the chosen word
+document.addEventListener("keypress", function (event) {
+    var keyword = String.fromCharCode(event.keyCode);
+    // if user guess is right
+    console.log(keyword);
+    if (chosenWord.indexOf(keyword) > -1) {
+        // add to rightGuess array
+        matchedLetters.push(keyword.toUpperCase());
+        console.log(matchedLetters);
+        // and replace underscore with right letter
+        // underScores[chosenWord.indexOf(keyword)] = keyword;
+        //docUnderScore[0].innerHTML = underScores.join(" ");
+        //docRightGuess.innerHTML = matchedLetters;
+        // Checks to see if user completes the word
+        //   if (underScore.join(" ") == chosenWord) {
+        //      alert("you win");
+        //}    
+    }
+    else {
+        guessedLetters.push(keyword.toUpperCase());
+        console.log(guessedLetters);
+        //docWrongGuess.innerHTML = guessedLetters;
+    }
+})
