1431c753fd7bae1aa36ec86758af200618889dd4
Kris Renaldi <krenaldi@comcast.net>
2019-06-12 16:25:20 -0400

    Able to get guesses to appear in their respective boxes on page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // ############ GLOBAL VARIABLES ###############
 // Create array that computer uses to select for play
-var words = ["Shinedown", "Rival Sons", "Greta Van Fleet"];
+var words = ["shinedown", "rival sons", "greta van fleet"];
 
 // Var used to show how many guesses user has left
 var guessesLeft = 6;
@@ -21,9 +21,9 @@ console.log(chosenWord);
 
 
 // Variables for DOM manipulation
-//var docUnderScore = document.querySelector("underscores");
-//var docRightGuess = document.querySelector("rightguess");
-//var docWrongGuess = document.querySelector("wrongguess");
+var docUnderScore = document.querySelector("#underscores");
+var docRightGuess = document.querySelector("#rightguess");
+var docWrongGuess = document.querySelector("#wrongguess");
 
 // ############ MAIN GAME ENGINE ##############
 // Create underscores based on length of word & update based on user's guesses
@@ -43,7 +43,7 @@ function generateUnderscore() {
         }
     }
     // Update the page with the new string we built.
-    document.querySelector("#underscores").innerHTML = wordLetters;
+    docUnderScore.innerHTML = wordLetters;
 }
 
 function gameStart() {
@@ -63,20 +63,45 @@ document.addEventListener("keypress", function (event) {
     console.log(keyword);
     if (chosenWord.indexOf(keyword) > -1) {
         // add to rightGuess array
-        matchedLetters.push(keyword.toUpperCase());
+        matchedLetters.push(keyword);
         console.log(matchedLetters);
         // and replace underscore with right letter
-        // underScores[chosenWord.indexOf(keyword)] = keyword;
-        //docUnderScore[0].innerHTML = underScores.join(" ");
-        //docRightGuess.innerHTML = matchedLetters;
+        underScores[chosenWord.indexOf(keyword)] = keyword;
+        docUnderScore.innerHTML = underScores.join(" ");
+        docRightGuess.innerHTML = matchedLetters;
         // Checks to see if user completes the word
         //   if (underScore.join(" ") == chosenWord) {
         //      alert("you win");
         //}    
     }
     else {
-        guessedLetters.push(keyword.toUpperCase());
+        guessedLetters.push(keyword);
         console.log(guessedLetters);
-        //docWrongGuess.innerHTML = guessedLetters;
+        docWrongGuess.innerHTML = guessedLetters;
     }
 })
+
+  // This function is run whenever the user guesses a letter..
+  function updatePage(letter) {
+    // If the user has no guesses left, restart the game.
+    //if (this.guessesLeft === 0) {
+     // this.restartGame();
+    //}
+    // Otherwise...
+    //else {
+      // Check for and handle incorrect guesses.
+      updateGuesses(letter);
+
+      // Check for and handle correct guesses.
+      updateMatchedLetters(letter);
+
+      // Rebuild the view of the word. Guessed letters are revealed, non-guessed letters have a "_".
+      generateUnderscore();
+
+      // If the user wins, restart the game.
+    //   if (this.updateWins() === true) {
+    //     this.restartGame();
+    //   }
+    //}
+
+}
